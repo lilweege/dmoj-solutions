3d1f9da901520b9e6bde35c49922b7e0ad901c47
Luigi Quattrociocchi <lquatt2@gmail.com>
2021-07-23 21:29:03 -0400

    bofa

--- BofaReader.java
+++ BofaReader.java
@@ -1,14 +1,15 @@
 import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.IOException;
 
 class BofaReader {
 	private BufferedReader br;
 	private StringTokenizer st;
 	
-	public BofaReader(InputStreamReader streamReader) {
-		br = new BufferedReader(streamReader);
+	public BofaReader(InputStream inStream) {
+		br = new BufferedReader(new InputStreamReader(inStream));
 	}
 
 	public String next() throws IOException {
