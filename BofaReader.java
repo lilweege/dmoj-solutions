9c3fc1210da49c7e6f240fed40e6aef941cdbc82
Luigi Quattrociocchi <lquatt2@gmail.com>
2021-08-18 20:01:36 -0400

    config

--- BofaReader.java
+++ BofaReader.java
@@ -1,40 +1,114 @@
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.DataInputStream;
 
 class BofaReader {
-	private BufferedReader br;
-	private StringTokenizer st;
-	
-	public BofaReader(InputStream inStream) {
-		br = new BufferedReader(new InputStreamReader(inStream));
+	final private int BUFFER_SIZE = 1 << 16;
+	private DataInputStream din;
+	private byte[] buffer;
+	private int bufferPointer, bytesRead;
+
+	public BofaReader() {
+		din = new DataInputStream(System.in);
+		buffer = new byte[BUFFER_SIZE];
+		bufferPointer = bytesRead = 0;
 	}
 
-	public String next() throws IOException {
-		while (st == null || !st.hasMoreTokens())
-			st = new StringTokenizer(br.readLine().trim());
-		return st.nextToken();
+	public BofaReader(String file_name) throws IOException {
+		din = new DataInputStream(new FileInputStream(file_name));
+		buffer = new byte[BUFFER_SIZE];
+		bufferPointer = bytesRead = 0;
 	}
 
-	public long readLong() throws IOException {
-		return Long.parseLong(next());
-	}
-
-	public char readCharacter() throws IOException {
-		return next().charAt(0);
-	}
-
-	public int readInt() throws IOException {
-		return Integer.parseInt(next());
-	}
-
-	public double readDouble() throws IOException {
-		return Double.parseDouble(next());
+	public String read() throws IOException {
+		byte[] ret = new byte[64];
+		int idx = 0;
+		byte c = Read();
+		while (c <= ' ') {
+			c = Read();
+		}
+		do {
+			ret[idx++] = c;
+			c = Read();
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		return new String(ret, 0, idx);
 	}
 
 	public String readLine() throws IOException {
-		return br.readLine().trim();
+		byte[] buf = new byte[64];
+		int cnt = 0, c; 
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		} 
+		return new String(buf, 0, cnt); 
+	}
+
+	public int nextInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		}  while ((c = Read()) >= '0' && c <= '9');
+		return neg ? -ret : ret;
+	} 
+
+	public long nextLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		return neg ? -ret : ret;
+	}
+
+	public double nextDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.')
+			while ((c = Read()) >= '0' && c <= '9')
+				ret += (c - '0') / (div *= 10);
+
+		return neg ? -ret : ret;
+	}
+
+	private void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
 	}
 }
